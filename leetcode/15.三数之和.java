d072d6082ef29cd3df9eee6db35bbdefc2f669c1
Han Rain <rain.han@shopee.com>
2022-05-18 15:53:12 +0800

    no.15

---
+++ leetcode/15.三数之和.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=15 lang=java
+ *
+ * [15] 三数之和
+ *
+ * https://leetcode.cn/problems/3sum/description/
+ *
+ * algorithms
+ * Medium (35.45%)
+ * Likes:    4784
+ * Dislikes: 0
+ * Total Accepted:    970.2K
+ * Total Submissions: 2.7M
+ * Testcase Example:  '[-1,0,1,2,-1,-4]'
+ *
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0
+ * 且不重复的三元组。
+ * 
+ * 注意：答案中不可以包含重复的三元组。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [0]
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * -10^5 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if(nums[i]>0)
+                break;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int l = i + 1, r = nums.length - 1, target = 0 - nums[i];
+            while (l < r) {
+                if (nums[l] + nums[r] == target) {
+                    List tmp = new ArrayList<Integer>();
+                    tmp.add(nums[i]);
+                    tmp.add(nums[l]);
+                    tmp.add(nums[r]);
+                    res.add(tmp);
+
+                    while (l < r && nums[l] == nums[l + 1])
+                        l++;
+                    while (l < r && nums[r] == nums[r - 1])
+                        r--;
+                    l++;
+                    r--;
+                } else if (nums[l] + nums[r] > target) {
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+        return res;
+    }
+
+}
+// @lc code=end
+/*
+    排序,注意第一个初始值的重复
+    然后就是l和r的重复，要判断位置，再调整位置
+
+*/
